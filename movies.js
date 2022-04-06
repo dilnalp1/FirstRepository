7eecb6f360cfa0c4080a80ef957d3883cce86292
dilnalp1 <100578333+dilnalp1@users.noreply.github.com>
2022-04-06 15:57:38 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -6,36 +6,27 @@ var updateCallback = function(data){
     console.log("line : "+line);
     var movieName = line.text;
     if (line.source.toLowerCase()==="visitor"){
-    var url = "https://www.omdbapi.com?t="+movieName+"&apikey=32278a3";
-    
-   
-    
+    var url = "https://www.omdbapi.com?t="+movieName+"&apikey=836dcef";
+       
     fetch(url)
     .then(function(response){
-    
-    return response.json();
+        return response.json();
     }
     ).then(function(res){
     document.getElementById("Title").innerHTML = res.Title;
     document.getElementById("Year").innerHTML = res.Year;
     document.getElementById("Genre").innerHTML = res.Genre;
-    document.getElementById("Plot").innerHTML = res.Plot;
-    
-    
-    
+    document.getElementById("Plot").innerHTML = res.Plot;   
     }).catch(function(error){
     console.log("Error Message : "+error);
     })
     }
     };
-    
-    
-    
+   
     var notifyWhenDone = function(error) {
     if (err){
     console.log("Error Occured in notify : "+err);
     }
-    
     errorMessage.innerHTML = "Unable to find the movie";
     };
     
